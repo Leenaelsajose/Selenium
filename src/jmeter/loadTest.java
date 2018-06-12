56bf683cf052d4424f712831ea53220ad20c99db
megin <megin@192.168.0.16>
2018-06-12 15:00:54 -0400

    committing load test

---
+++ src/jmeter/loadTest.java
@@ -0,0 +1,23 @@
+package jmeter;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class loadTest {
+	
+	@Test
+	public void testing()
+	{
+		System.setProperty("webdriver.chrome.driver", "C://Leena/selenium_drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://ebay.com");
+		System.out.println(driver.getTitle());
+		
+		
+		
+	}
+	
+	
+
+}
