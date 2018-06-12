53b405f30f150eb2063fc118ee75ce36bfd57c39
Leenaelsajose <38896754+Leenaelsajose@users.noreply.github.com>
2018-06-12 15:40:00 -0400

    Update loadTest.java

--- src/jmeter/loadTest.java
+++ src/jmeter/loadTest.java
@@ -13,7 +13,7 @@ public class loadTest {
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://ebay.com");
 		System.out.println(driver.getTitle());
-		System.out.println(driver.getTitle());
+		
 		
 		
 	}
